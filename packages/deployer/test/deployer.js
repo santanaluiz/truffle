500c935b8fdb5f270371c04a7d0ef54c9fb07db6
eggplantzzz <tmfeickert@gmail.com>
2021-11-12 12:06:51 -0500

    Update some more tests and remove confirmations from reporter

--- packages/deployer/test/deployer.js
+++ packages/deployer/test/deployer.js
@@ -42,7 +42,7 @@ const linkingOccurredForName = (options, contractName, libraryName) => {
 };
 // end test helpers
 
-describe.only("Deployer (sync)", function() {
+describe("Deployer (sync)", function() {
   let owner;
   let options;
   let networkId;
@@ -102,9 +102,16 @@ describe.only("Deployer (sync)", function() {
           "migrate:deployment:txHash": [],
           "migrate:deployment:postDeploy": [],
           "migrate:deployment:linking": [],
+          "migrate:deployment:confirmation": [],
         },
         emit: function(eventName, data) {
-          options.events.emittedEvents[eventName].push(data);
+          if (options.events.emittedEvents[eventName]) {
+            options.events.emittedEvents[eventName].push(data);
+          } else {
+            console.log(
+              `Could not find the event name ${eventName} in 'emittedEvents'.`;
+            );
+          }
         }
       }
     };
